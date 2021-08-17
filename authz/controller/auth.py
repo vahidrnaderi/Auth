7d698cddaed8b68cf04772ebf6c0d3ec38998b00
Vahid Naderi <vahidrnaderi@gmail.com>
2021-08-17 21:13:06 +0430

    adding endpoint for authentication

---
+++ authz/controller/auth.py
@@ -0,0 +1,71 @@
+from flask import abort, request
+from flask.json import dump
+from jwt import encode
+from time import time
+
+from authz import db
+from authz.config import Config
+from authz.model import User
+from authz.schema import UserSchema
+from authz.util import now
+
+class AuthTokenController:
+
+    def create_token():
+        if request.content_type != "application/json":
+            cbort(415) # Bad media type.
+        user_schema = UserSchema(only=["username", "password"])
+        try:
+            data = user_schema.load(request.get_json()) # Vaidate request data.
+        except:
+            abort(400)
+        if not data ["username"] or not data["password"]:
+            abort(400)
+        try:
+            user = User.query.filter_by(username=data["username"]).first() # Select a user.
+        except:
+            abort(500) # Database error.
+        if user is None:
+            abort(400) # User is not found.
+        if user.password != data["password"]:
+            user.last_failed_at = now()
+            try:
+                db.session.commit()
+            except:
+                db.session.rollback()
+                abort(500) # Database error.
+            abort(403) # Incorrect password.
+        current_time = time()
+        try:
+            jwt_token = encode(
+                    {
+                        "nbf": current_time,
+                        "exp": current_time + Config.JWT_TOKEN_LIFETIME,
+                        "user": {
+                            "id": user.id,
+                            "username": user.username,
+                            "role": user.role
+                            }
+                        },
+                        Config.SECRET,
+                        algorithm=Config.JWT_ALGO
+                    ) #Create new jwt token.
+        except:
+            abort(500) # JWT encode error.
+        user_schema = UserSchema()
+        return {
+                "user": user_schema.dump(user)
+                }, 201, {"X_Subject-Token": jwt_token}
+
+
+
+
+
+
+
+
+
+
+
+
+
